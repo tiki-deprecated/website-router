70565fa05f32150e4396c6f4f579f9c9c5e0bd16
Mike Audi <mike@mytiki.com>
2023-04-03 00:11:46 -0500

    feat!: setup simple proxy to route api req

---
+++ src/worker-configuration.d.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+interface Env {
+  AUTHORIZE_PATH: string;
+  AUTHORIZE_L0_URL: string;
+
+  LIST_LICENSE_PATH: string;
+  LIST_LICENSE_L0_URL: string;
+
+  LICENSE_PATH: string;
+  LICENSE_L0_URL: string;
+
+  TITLE_PATH: string;
+  TITLE_L0_URL: string;
+}
